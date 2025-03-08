f92a5136c979c347230a4c8aa48793b9e846a349
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-03-08 16:24:49 -0300

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import Hero from "../../components/Hero";
-import P5Background from "../../components/P5Background";
+
 
 
 
 export default function Home() {
   return (
     <>
-    <P5Background />
+
       <Hero />
       
       
